366a12321392217a557506215b57564a0d4e9166
lakindu999 <lakindusamarakoon@gmail.com>
2023-05-22 22:07:20 +0530

    UseHooks

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,28 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState,useEffect} from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs,setBlogs] = useState ([
-        {title: 'My new site', body:'lorem ipsum...', author:'Shashini', id:1},
-        {title: 'Welcome Party !', body:'lorem ipsum...', author:'Ramalka', id:2},
-        {title: 'Halo Halo', body:'lorem ipsum...', author:'Shanuka', id:3},
-    ]);
+    const [blogs,setBlogs] = useState(null);
+    const [isPending,setIsPending] = useState(true);
+
+    useEffect(()=>{
+      setTimeout(() =>{
+        fetch('http://localhost:8000/blogs')
+      .then(res =>{ 
+        return res.json();
+      })
+      .then(data=>{
+        setBlogs(data);
+        setIsPending(false);
+      });
+      }, 1000);
+    }, []);  
 
   return (
 <div className="home">
-    <BlogList blogs={blogs} title="All Blogs"/>
+    {isPending && <div>Loading.....</div>}
+    {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
 </div>
   )
 }
